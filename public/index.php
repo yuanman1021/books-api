4d9b97808cae6ba3d8e6c6f57573a18d1e90f933
yuanman1021 <khooyuanman@graduate.utm.my>
2026-05-20 12:27:24 +0800

    Update public/index.php

--- public/index.php
+++ public/index.php
@@ -6,6 +6,9 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
+$app->add(new App\Middleware\JsonBodyParser());
+$app->add(new App\Middleware\Cors());
+
 $app->addRoutingMiddleware();
 
 $app->addErrorMiddleware(true, true, true);
