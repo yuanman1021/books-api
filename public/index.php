99a58e1eaa71f47e2fa9abd74ea97e2f1aeb65b7
yuanman1021 <khooyuanman@graduate.utm.my>
2026-05-20 12:22:56 +0800

    part D: build the books resource

--- public/index.php
+++ public/index.php
@@ -1,7 +1,5 @@
 <?php
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
@@ -9,11 +7,9 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = AppFactory::create();
 
 $app->addRoutingMiddleware();
+
 $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', function (Request $req, Response $res) {
-    $res->getBody()->write('Hello, Slim!');
-    return $res;
-});
+(require __DIR__ . '/../src/routes.php')($app);
 
 $app->run();
