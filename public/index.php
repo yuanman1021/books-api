52711dcfb2a1e8956d60f995f3a5de40481a8977
yuanman1021 <khooyuanman@graduate.utm.my>
2026-05-20 12:17:14 +0800

    part C: "Hello, Slim!"

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->addRoutingMiddleware();
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function (Request $req, Response $res) {
+    $res->getBody()->write('Hello, Slim!');
+    return $res;
+});
+
+$app->run();
